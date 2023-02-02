ef28bb67b2e1a581748b9077cb4952501a9c2408
eydellnet08 <eydellnet08@EYDNET24>
2023-02-02 14:21:39 +0530

    Add project files.

---
+++ Subtraction_Feb2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subtraction_Feb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum, secondNum, result;
+            Console.WriteLine("Enter the numbers to sub :");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            result = (firstNum - secondNum);
+            Console.WriteLine("Result =" + result);
+
+        }
+    }
+}
